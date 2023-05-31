720361f5717c07e5ffe24a2b18b9f2f097c31e15
Stanislav <stanislav1502@abv.bg>
2023-05-31 13:43:42 +0300

    Added diploma controler simple diploma views link to diploma page in navbar

---
+++ DiplomaSite3/Controllers/DiplomaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace DiplomaSite3.Controllers
+{
+    public class DiplomaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int nTimes=1)
+        {
+            ViewData["Message"] = "Welcome " + name;
+            ViewData["numtimes"] = nTimes;
+            return View();
+        }
+    }
+}
